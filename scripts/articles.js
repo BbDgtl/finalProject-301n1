9d40749227fdc492cad6d32d13029c65cfae6c6f
BbDgtl <bbdgtl@gmail.com>
2016-02-16 20:26:05 -0800

    class-17

--- scripts/articles.js
+++ scripts/articles.js
@@ -1,1 +1,49 @@
-//******* 02/16/2016 Database of EDM musicians playing in Seattle, WA. ******
+//******* March 2016 Database of EDM musicians playing in Seattle, WA. ******
+//Create a simple admin page to add venue details more efficiently
+var showData = [
+  {
+  'dateTime': '2016-03-01T20:00:00'
+  'artist': 'Hippie Sabotage',
+  'venue': 'Showbox Market'
+  'address': '1426 1st Ave Seattle, WA 98101'
+  'coordinates': '47.6085082,-122.3416781'
+  'venueUrl': 'www.showboxpresents.com'
+  'details': 'All Ages to Enter, 21 & Over to Drink'
+  },
+  {
+  'dateTime': '2016-03-02T19:00:00'
+  'artist': 'Galantis/USC'
+  'venue': 'Showbox Sodo'
+  'address': '1700 1st Ave S Seattle, WA 98134'
+  'coordinates': '47.587935,-122.3358859'
+  'venueUrl': 'www.showboxpresents.com'
+  'details': '<p>[SOLD OUT] 18+ <a href="www.facebook.com/events/202549353411389/">Facebook Events Page</a></p> <p>Widely known as the Swedish super-duo of the decade, the fiery feline group Galantis has been taking clubs and festivals by storm. Representing a vibe that can be described as “the place that EDM and pop meet” has landed them top spots on Electronic hit lists over the past few years. Don’t miss Galantis taking over the Showbox SoDo March 2nd!</p>'
+  },
+  {
+  'dateTime': '2016-03-02T22:00:00'
+  'artist': 'Gladiator/SUBstance: Louder Harder Better Tour'
+  'venue': 'Foundation Nightclub'
+  'address': '2218 Western Ave \#100 Seattle, WA 98121'
+  'coordinates': '47.6123987,-122.3486207'
+  'venueUrl': 'www.foundation-nightclub.com'
+  'details': '<p>21+ Free admission with student ID</p> <p>SUBstance Wednesdays drink specials: 10-11pm All wells $3 11-12pm All wells $5 $3 Rolling Rock Tall Boys before 11pm</p> <p>Constantly evolving, spawning new sub-genres, and reaching into uncharted depths of the frequency range; bass music has been ingrained in the Seattle scene for well over two decades. Join us every Wednesday as we fuse the sounds of Dubstep, Drum and Bass, Trap, Moombahton, Glitch Hop, Drumstep, and everything in between to write the next chapter in this region’s bass music saga. No frills, no fluff, just industry champions and dedicated locals pushing our sound system to its limits every week. This is SUBstance.</p> <p>About Gladiator: <a href="www.gladiator-music.com"></a> <a href="soundcloud.com/gladiator"></a></p> <p>Born out of a passion for DJing, Gladiator was conceived in 2010 by Los Angeles natives Daniel Goodman and Ian Johnson. The duo is known for bringing an irresistible sense of fun and passion to every show with their unique genre-bending sets. After debuting with a series of self-released internet uploads and bootleg remixes, Gladiator joined the A-Trak led label Fool’s \Gold and released their first official EP Assembly Line in 2014. Following this release they found the opportunity to make official remixes for the likes of Low Pros & Juvenile’s “Muscle,” Skrillex’s “Dirty Vibe” and Melanie Martinez’s “Soap,” as well as singles such as “Cruise Control” and “Cute” with fellow Fool’s \Gold artists Sleepy Tom and Kittens.</p>'
+  },
+  {
+  'dateTime': '2016-03-04T20:00:00'
+  'artist': 'Animal Collective'
+  'venue': 'Neptune Theatres'
+  'address': '1303 NE 45th St Seattle, WA 98105'
+  'coordinates': '47.6611709,-122.31621'
+  'venueUrl': 'www.stgpresents.org'
+  'details': '<p>Animal Collective is pleased to announce an extensive worldwide tour in 2016, in support of their recently announced new album, Painting With, out February 19th on Domino. Starting in Philadelphia on album release day, the dates across North America and Europe will be the first live Animal Collective shows since 2013.</p> <p>Animal Collective is an American experimental pop group formed in Baltimore, Maryland in 1999. They were founded by Avey Tare (David Portner), Panda Bear (Noah Lennox), Deakin (Josh Dibb), and Geologist (Brian Weitz). Records released under the name Animal Collective may include contributions from any or all of these members; the lineup is not uniform, though Portner and Lennox have been on every Animal Collective release.</p> <p>The band’s music is characterized by studio experimentation, vocal harmonies, and an exploration of various genres which include freak folk, noise rock, ambient drone, and psychedelia. The group also runs the record label Paw Tracks on which they have released their own material as well as that of other artists. In 2009, the band released their most commercially successful album, Merriweather Post Pavilion, which Uncut magazine called: “one of the landmark American albums of the century so far”.</p><p>With special guest Ratking</p><p>Doors at 8:00 pm, Show at 9:00 pm</p><p>All Ages / Bar with ID</p><p><a href="www.facebook.com/events/413598415505048/">Facebook Event Page</a></p>'
+  },
+  {
+  'dateTime': ''
+  'artist': ''
+  'venue': ''
+  'address': ''
+  'coordinates': ''
+  'venueUrl': ''
+  'details': ''
+  },
+];
